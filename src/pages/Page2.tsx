04744e82f1bc34fb7e517d0c1b832289416483aa
Micah Rairdon <mrairdon@midmark.com>
2022-03-24 12:15:37 -0400

    Adding dep injection to page 2

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,4 +1,13 @@
+import { useContext } from 'react';
+import { MyServiceContext } from '..';
+
 export function Page2() {
-    return <div>Page 2</div>;
+  const myService = useContext(MyServiceContext);
+  return (
+    <div>
+      <h2>Page 2</h2>
+      <p>Dependency injection magic: {myService && myService()}</p>
+    </div>
+  );
   }
   
